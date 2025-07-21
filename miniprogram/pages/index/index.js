17bb47ff49d65ea5493569b6aab261faab77ce08
hunter <1603668353@qq.com>
2025-07-22 00:49:26 +0800

    feat: Added notices

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,8 +1,68 @@
 const { init } = require('@cloudbase/wx-cloud-client-sdk')
 const client = init(wx.cloud)
 const models = client.models
+
 Page({
   data: {
+    announcement_list: [
+      {
+        "images":"/resources/images/test/广告通知测试.png",
+        "desc": "这是一个通知",
+        "type": "通知"
+      },
+      {
+        "images":"/resources/images/test/广告通知测试.png",
+        "desc": "这是一个广告",
+        "type": "广告"
+      },
+      {
+        "images":"/resources/images/test/广告通知测试.png",
+        "desc": "这是一个广告",
+        "type": "广告"
+      },
+      {
+        "images":"/resources/images/test/广告通知测试.png",
+        "desc": "这是一个广告",
+        "type": "广告"
+      }
+    
+    
+    ],
+    notices: {
+      class: [
+        {
+          "text": "08:30",
+          "desc": "电路分析"
+        },
+        {
+          "text": "09:30",
+          "desc": "计算机组成原理"
+        },
+        {
+          "text": "10:30",
+          "desc": "嵌入式"
+        },
+        {
+          "text": "11:30",
+          "desc": "Python"
+        },
+        {
+          "text": "13:30",
+          "desc": "数据结构"
+        },
+        {
+          "text": "15:30",
+          "desc": "C语言"
+        },
+        {
+          "text": "17:30",
+          "desc": "C++"
+        },
+      ],
+      life: [
+
+      ]
+    },
     menuPosition: wx.getMenuButtonBoundingClientRect(),
     menuItems: [
       {
